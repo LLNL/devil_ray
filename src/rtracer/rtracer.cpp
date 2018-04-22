3d6a9a4a3753c22357d9a08fea29b264d03a6097
Matt Larsen <larsen30@llnl.gov>
2018-04-21 21:30:24 -0700

    adding missing files

---
+++ src/rtracer/rtracer.cpp
@@ -0,0 +1,25 @@
+#include <rtracer.hpp>
+#include <iostream>
+
+namespace rtracer
+{
+
+void
+rtracer::init()
+{
+
+}
+
+void
+rtracer::finalize()
+{
+
+}
+
+void
+rtracer::about()
+{
+  std::cout<<"rtracer v0.0.1\n";
+}
+
+} // namespace rtracer
---
+++ src/rtracer/rtracer.hpp
@@ -0,0 +1,14 @@
+#ifndef RTRACER_HPP
+#define RTRACER_HPP
+namespace rtracer
+{
+class rtracer 
+{
+  public:
+    void about();
+    void init();
+    void finalize();
+};
+
+} // namespace rtracer
+#endif
