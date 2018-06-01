92f9c51852b3750efc1ec4fad69dd87a77214bd3
Masado Ishii <ishii3@llnl.gov>
2018-06-01 11:59:38 -0700

    Populate an array with rand()om integer values.
    For now, it is implemented serially.

--- src/dray/array_utils.hpp
+++ src/dray/array_utils.hpp
@@ -71,6 +71,51 @@ Array<int32> array_counting(const int32 &size,
   return iterator;
 }
 
+static
+Array<int32> array_random(const int32 &size,
+                          const uint64 &seed,
+                          const int32 &modulus)
+{
+  // I wanted to use both 'seed' and 'sequence number' (see CUDA curand).
+  // The caller provides seed, which is shared by all array elements;
+  // but different array elements have different sequence numbers.
+  // The sequence numbers should advance by (size) on successive calls.
+
+  // For the serial case, I will instead use a "call number" and change the seed,
+  // by feeding (given seed + call number) into the random number generator.
+  // Unfortunately, two arrays each of size N will get different entries
+  // than one array of size 2N.
+
+  static uint64 call_number = 1;    // Not 0: Avoid calling srand() with 0 and then 1.
+  //static uint64 sequence_start = 0;    //future: for parallel random
+
+  // Allocate the array.
+  Array<int32> rand_array;
+  rand_array.resize(size);
+  //int32 *ptr = rand_array.get_device_ptr();
+  int32 *host_ptr = rand_array.get_host_ptr();
+
+  // Initialize serial random number generator, then fill array.
+  srand(seed + call_number);
+  for (int32 i = 0; i < size; i++)
+    host_ptr[i] = rand() % modulus;
+  
+
+  // TODO parallel random number generation
+//  RAJA::forall<for_policy>(RAJA::RangeSegment(0, size), [=] DRAY_LAMBDA (int32 i)
+//  {
+//    curandState_t state;
+//    curand_init(seed, sequence_start + i, 0, &state);
+//    ptr[i] = curand(&state);
+//  });
+
+  call_number++;
+  //sequence_start += size;
+
+  return rand_array;
+}
+
+
 #ifdef __CUDACC__
 inline __device__
 Vec<float32,4> const_get_vec4f(const Vec<float32,4> *const data)
