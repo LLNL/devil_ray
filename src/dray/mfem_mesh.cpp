f37287a01a4efdf6d8285def00f18b4c81aea61a
larsen30 <larsen30@vpnb-user-128-15-245-104.llnl.gov>
2018-06-30 03:54:58 -0700

    adding work around for NURBS race mfem condition

--- src/dray/mfem_mesh.cpp
+++ src/dray/mfem_mesh.cpp
@@ -203,6 +203,12 @@ MFEMMesh::MFEMMesh(mfem::Mesh *mesh)
   assert(mesh->Dimension() == 3);
 
   m_mesh = mesh;
+
+   if (m_mesh->NURBSext)
+   {
+      m_mesh->SetCurvature(2);
+   }
+
   m_is_high_order =
      (mesh->GetNodalFESpace() != nullptr) && (mesh->GetNE() > 0);
 
