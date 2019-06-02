1ae84c2a5e95849af04b4f0d1cb51e81f2f6a6ab
mclarsen <larsen30@llnl.gov>
2019-06-02 14:31:26 -0700

    removing dead tests

--- src/tests/dray/t_dray_appstats.cpp
+++ src/tests/dray/t_dray_appstats.cpp
@@ -4,7 +4,7 @@
 #include <dray/utils/appstats.hpp>
 #include <dray/utils/global_share.hpp>
 
-TEST(dray_mfem_blueprint, dray_mfem_blueprint)  //TODO change mfem_blueprint to something that makes sense?
+TEST(dray_stats, dray_stats_smoke)
 {
 
   std::shared_ptr<dray::stats::AppStats> app_stats_ptr = dray::stats::global_app_stats.get_shared_ptr();
