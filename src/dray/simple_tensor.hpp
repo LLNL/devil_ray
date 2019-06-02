27af77948699fc3f065d066cc09a95a554b4b2f8
mclarsen <larsen30@llnl.gov>
2019-06-02 15:01:36 -0700

    bypassing bad mem accesses in test

--- src/dray/simple_tensor.hpp
+++ src/dray/simple_tensor.hpp
@@ -39,8 +39,10 @@ struct SimpleTensor   // This means single product of some number of vectors.
 
   DRAY_EXEC int32 get_size_tensor(int32 t_order) { return pow(s,t_order); }
 
-  // Where you should store the vectors that will be used to construct the tensor.
-  // First pointer is for the last/outermost looping index variable in the tensor: X1(Y1Y2Y3)X2(Y1Y2Y3)X3(Y1Y2Y3).
+  // Where you should store the vectors that will
+  // be used to construct the tensor.
+  // First pointer is for the last/outermost looping
+  // index variable in the tensor: X1(Y1Y2Y3)X2(Y1Y2Y3)X3(Y1Y2Y3).
   DRAY_EXEC void get_vec_init_ptrs(int32 t_order, T *arr, T **ptrs)
   {
     // We align all the initial vectors in the same direction,
@@ -58,9 +60,9 @@ struct SimpleTensor   // This means single product of some number of vectors.
   DRAY_EXEC int32 construct_in_place(int32 t_order, T *arr)
   {
     // This is a recursive method.
-    if (t_order == 0) 
-    { 
-      return s; 
+    if (t_order == 0)
+    {
+      return s;
     }
     else
     {
