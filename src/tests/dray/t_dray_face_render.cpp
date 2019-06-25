6332992b96d344ba53982fa4854e8a6c5f407078
Masado Ishii <ishii3@llnl.gov>
2019-06-25 09:50:46 -0700

    Cleaned up unit test a little

--- src/tests/dray/t_dray_face_render.cpp
+++ src/tests/dray/t_dray_face_render.cpp
@@ -105,53 +105,20 @@ TEST(dray_volume_render, dray_volume_render_simple)
   dray::Array<dray::ray32> rays;
   camera.create_rays(rays);
 
+  dray::BVH scene_bvh = dray::LinearBVHBuilder().construct(mesh.get_aabbs());
+
   //
   // Mesh faces rendering
   //
-  dray::BVH scene_bvh = dray::LinearBVHBuilder().construct(mesh.get_aabbs());
-  dray::Array<dray::Vec<dray::float32,4>> color_buffer = dray::mesh_lines<dray::float32>(rays, mesh, scene_bvh);
-
-  //
-  // Volume rendering
-  //
-
   {
+    dray::Array<dray::Vec<dray::float32,4>> color_buffer = dray::mesh_lines<dray::float32>(rays, mesh, scene_bvh);
+
     dray::PNGEncoder png_encoder;
     png_encoder.encode( (float *) color_buffer.get_host_ptr(), camera.get_width(), camera.get_height() );
     png_encoder.save(output_file + ".png");
     EXPECT_TRUE(check_test_image(output_file));
   }
 
-#if 0
-   //
-   // Isosurface
-   //
-
-  camera.create_rays(rays);
-
-  // Output isosurface, colorized by field spatial gradient magnitude.
-  {
-    float isovalues[5] = { 0.07, 0.005, 0, -8, -15 };
-    const char* filenames[5] = {"isosurface_001.png",
-                                "isosurface_+08.png",
-                                "isosurface__00.png",
-                                "isosurface_-08.png",
-                                "isosurface_-15.png"};
-
-    for (int iso_idx = 0; iso_idx < 1; iso_idx++)
-    {
-      std::cout<<"doing iso_surface "<<iso_idx<<" size "<<rays.size()<<"\n";
-      dray::Array<dray::Vec4f> color_buffer = mesh_field.isosurface_gradient(rays, isovalues[iso_idx]);
-      std::cout<<"done doing iso_surface "<<"\n";
-      dray::PNGEncoder png_encoder;
-      png_encoder.encode( (float *) color_buffer.get_host_ptr(), camera.get_width(), camera.get_height() );
-      png_encoder.save(filenames[iso_idx]);
-
-      printf("Finished rendering isosurface idx %d\n", iso_idx);
-    }
-  }
-  // --- end DRAY  --- //
-#endif
 }
 
 
