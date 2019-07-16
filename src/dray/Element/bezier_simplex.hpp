41ad2febeb94c81181fd5fe74d516e86662a412f
Masado Ishii <ishii3@llnl.gov>
2019-07-15 18:32:08 -0700

    sampling points in the reference triangle and rasterizing them as points (to later test triangle eval)

---
+++ src/dray/Element/bezier_simplex.hpp
@@ -0,0 +1,165 @@
+#ifndef DRAY_BEZIER_SIMPLEX_HPP
+#define DRAY_BEZIER_SIMPLEX_HPP
+
+#include <dray/exports.hpp>
+#include <dray/types.hpp>
+#include <dray/vec.hpp>
+
+namespace dray
+{
+
+//TODO put into a new multinomial class file.
+template <int32 dim>
+class MultinomialCoeff
+{
+  // Invariant: m_val = MultinomialCoeff(n; i, j, k).
+  // Invariant: i+j+k = n.
+  protected:
+    int32 m_val;
+    int32 m_n;
+    int32 m_ijk[dim + 1];
+
+  public:
+    // Before using MultinomialCoeff, call construct(n).
+    void construct(int32 n)
+    {
+      constexpr int32 full_place = dim;
+      m_val = 1;
+      m_n = n;
+      for (int32 d = 0; d <= dim; d++)
+        m_ijk[d] = 0;
+      m_ijk[full_place] = n;
+    }
+
+    // Getters.
+    int32        get_val() const { return m_val; }
+    int32        get_n()   const { return m_n; }
+    const int32 *get_ijk() const { return m_ijk; }
+
+    // slice_over() - Advance to next coefficient along a direction.
+    int32 slide_over(int32 inc_place)
+    {
+      constexpr int32 dec_place = dim;
+      //       n!              n!         k
+      // ---------------  =  -------  *  ---
+      // (i+1)! M (k-1)!     i! M k!      i
+      m_val *= m_ijk[dec_place]--;
+      m_val /= m_ijk[inc_place]++;
+      return m_val;
+    }
+
+    // swap_places() - The multinomial coefficient is symmetric in i, j, k.
+    void swap_places(int32 place1, int32 place2)
+    {
+      const int32 s = m_ijk[place2];
+      m_ijk[place2] = m_ijk[place1];
+      m_ijk[place1] = s;
+    }
+};
+
+
+template <typename T, int32 dim>
+struct BezierSimplex
+{
+  //TODO
+};
+
+
+
+/*
+ * Example
+ *
+ * Dofs for a quadratic triangular element form a triangle.
+ *
+ * Barycentric coordinates u + v + t = 1.
+ *
+ * To evaluate an element using Horner's rule, while accessing dofs
+ * in the lexicographic order of (u fastest, v next fastest),
+ * express the summation as follows:
+ *
+ *           7  []   \`           v^2 * {  Mck(2;0,2,0)*u^0 }
+ *          /         \`
+ *    'v'  /           \` (t=0)
+ *        /  []    []   \`        v^1 * { [Mck(2;0,1,1)*u^0]*t + Mck(2;1,1,0)*u^1 }
+ *       /               \`
+ *      /                 \`
+ *        []    []    []          v^0 * { [[Mck(2;0,0,2)*u^0]*t + Mck(2;1,0,1)*u^1]*t + Mck(2;2,0,0)*u^2 }
+ *  (t=1)
+ *        ------------->
+ *             'u'
+ *
+ *  where 'Mck' stands for M choose K, or multinomial coefficient.
+ *
+ *  Note that multinomial coefficients are symmetric in the indices.
+ *  In particular,
+ *      Mck(2; 0,0,2) == Mck(2; 2,0,0)
+ *      Mck(2; 0,1,1) == Mck(2; 1,1,0)
+ *  This property allows us to traverse Pascal's simplex using only
+ *  transitions between 'adjacent' multinomial coefficients.
+ */
+
+
+//
+// BezierSimplex<T,2> (Bezier triangle).
+//
+template <typename T>
+struct BezierSimplex<T,2>
+{
+  // eval(): Evaluation without computing derivatives.
+  template <typename DofT, typename PtrT = DofT*>
+  DRAY_EXEC static DofT eval(const Vec<T,2> &ref_coords, const PtrT dof_ptr, const int32 p)
+  {
+    // Barycentric coordinates.
+    const T &u = ref_coords[0];
+    const T &v = ref_coords[1];
+    const T t = T(1.0) - (u + v);
+
+    // Multinomial coefficient. Will traverse Pascal's simplex using
+    // transitions between adjacent multinomial coefficients (slide_over()),
+    // and transpositions back to the start of each row (swap_places()).
+    MultinomialCoeff<2> mck;
+    mck.construct(p);
+
+    DofT j_sum = 0;
+    T vpow = 1.0;
+    for (int32 jj = 0; jj <= p; jj++)
+    {
+
+      DofT i_sum = 0;
+      T upow = 1.0;
+      for (int32 ii = 0; ii <= (p-jj); ii++)
+      {
+        // Horner's rule innermost, due to decreasing powers of t (mu = p - jj - ii).
+        i_sum *= t;
+        i_sum += (*(dof_ptr++)) * (mck.get_val() * upow);
+        mck.slide_over(0);
+        upow *= u;
+      }
+      mck.swap_places(0,2);
+
+      j_sum += i_sum * vpow;
+      vpow *= v;
+      mck.slide_over(1);
+    }
+    //mck.swap_places(1,2);
+
+    return j_sum;
+  }
+
+};
+
+
+//
+// BezierSimplex<T,3> (Bezier tetrahedron).
+//
+template <typename T>
+struct BezierSimplex<T,3>
+{
+  //TODO
+};
+
+
+}//namespace dray
+
+
+#endif//DRAY_BEZIER_SIMPLEX_HPP
---
+++ src/tests/dray/t_dray_triangle.cpp
@@ -0,0 +1,33 @@
+#include "gtest/gtest.h"
+#include "test_config.h"
+/// #include "t_utils.hpp"
+
+#include <stdio.h>
+
+#include <dray/utils/png_encoder.hpp>
+#include <dray/filters/surface_triangle.hpp>
+
+TEST(dray_triangle, dray_triangle_single)
+{
+  std::string output_file = "triangle";
+  const int c_width = 1024;
+  const int c_height = 1024;
+
+  using Coord = dray::Vec<float,2>;
+  using Color = dray::Vec<float,4>;
+  dray::Array<Color> img_buffer;
+
+  // Define a triangle.
+  Coord nodes[3] = { {0.0f, 0.0f},
+                     {.75f, 0.0f},
+                     {0.0f, .75f},
+  };
+  dray::Array<Coord> nodes_array(nodes, 3);
+
+  img_buffer = dray::SurfaceTriangle().execute<float>(c_width, c_height, nodes_array, 1, 50000);
+
+  // Save image.
+  dray::PNGEncoder png_encoder;
+  png_encoder.encode( (float *) img_buffer.get_host_ptr(), c_width, c_height);
+  png_encoder.save(output_file + ".png");
+}
