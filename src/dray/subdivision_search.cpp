0b199343e8fddc8449dddb9e860acef64fa274eb
Masado Ishii <ishii3@llnl.gov>
2019-10-08 08:03:48 -0700

    Added missing file

---
+++ src/dray/subdivision_search.cpp
@@ -0,0 +1,9 @@
+#include <dray/subdivision_search.hpp>
+
+namespace dray
+{
+
+  /// template struct detail::SplitRefBox<AABB<2u>>;
+  /// template struct detail::SplitRefBox<AABB<3u>>;
+
+}//namespace dray
