a17955e033f290d80ccab69b8929708f671d3ac3
Matt Larsen <larsen30@llnl.gov>
2019-11-10 09:39:49 -0800

    moving bvh to its own file

--- src/dray/linear_bvh_builder.hpp
+++ src/dray/linear_bvh_builder.hpp
@@ -3,20 +3,10 @@
 
 #include <dray/array.hpp>
 #include <dray/aabb.hpp>
+#include <dray/bvh.hpp>
 
 namespace dray
 {
-
-struct BVH
-{
-  Array<Vec<float32, 4>> m_inner_nodes;
-  Array<int32>           m_leaf_nodes;
-  AABB<>                 m_bounds;
-  Array<int32>           m_aabb_ids;
-  // multiple leaf nodes can point to the same
-  // original primitive. m_aabb_ids point to the
-  // index of the aabb given to construct
-};
 
 class LinearBVHBuilder
 {
@@ -24,7 +14,6 @@ class LinearBVHBuilder
 public:
   BVH construct(Array<AABB<>> aabbs);
   BVH construct(Array<AABB<>> aabbs, Array<int32> primimitive_ids);
-
 };
 
 AABB<> reduce(const Array<AABB<>> &aabbs);
