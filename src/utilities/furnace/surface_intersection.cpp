116ea640aeb245d65b97401041b8cb399f84fa13
Matt Larsen <larsen30@llnl.gov>
2019-11-24 08:15:34 -0800

    format furnaces

--- src/utilities/furnace/surface_intersection.cpp
+++ src/utilities/furnace/surface_intersection.cpp
@@ -1,6 +1,6 @@
 #include <dray/dray.hpp>
-#include <dray/filters/mesh_lines.hpp>
 #include <dray/filters/mesh_boundary.hpp>
+#include <dray/filters/mesh_lines.hpp>
 #include <dray/utils/appstats.hpp>
 #include <dray/utils/png_encoder.hpp>
 
@@ -10,56 +10,55 @@
 #include <random>
 
 
-int main(int argc, char* argv[])
+int main (int argc, char *argv[])
 {
   std::string config_file = "";
 
-  if(argc != 2)
+  if (argc != 2)
   {
-    std::cout<<"Missing configure file name\n";
-    exit(1);
+    std::cout << "Missing configure file name\n";
+    exit (1);
   }
 
   config_file = argv[1];
 
-  Config config(config_file);
-  config.load_data();
-  config.load_camera();
-  config.load_field();
+  Config config (config_file);
+  config.load_data ();
+  config.load_camera ();
+  config.load_field ();
 
 
   using SMeshElemT = dray::MeshElem<2u, dray::ElemType::Quad, dray::Order::General>;
 
-  dray::DataSet<SMeshElemT> sdataset = dray::MeshBoundary().
-      template execute<MeshElemT>(config.m_dataset);
+  dray::DataSet<SMeshElemT> sdataset =
+  dray::MeshBoundary ().template execute<MeshElemT> (config.m_dataset);
 
 
   int trials = 5;
   // parse any custon info out of config
-  if(config.m_config.has_path("trials"))
+  if (config.m_config.has_path ("trials"))
   {
-    trials = config.m_config["trials"].to_int32();
+    trials = config.m_config["trials"].to_int32 ();
   }
 
   dray::Array<dray::Ray> rays;
-  config.m_camera.create_rays(rays);
+  config.m_camera.create_rays (rays);
 
   dray::MeshLines mesh_lines;
-  mesh_lines.set_field(config.m_field);
+  mesh_lines.set_field (config.m_field);
 
-  dray::Framebuffer framebuffer(config.m_camera.get_width(),
-                                config.m_camera.get_height());
+  dray::Framebuffer framebuffer (config.m_camera.get_width (),
+                                 config.m_camera.get_height ());
 
-  for(int i = 0; i < trials; ++i)
+  for (int i = 0; i < trials; ++i)
   {
-    framebuffer.clear();
-    config.m_camera.create_rays(rays);
-    mesh_lines.execute(rays, sdataset, framebuffer);
+    framebuffer.clear ();
+    config.m_camera.create_rays (rays);
+    mesh_lines.execute (rays, sdataset, framebuffer);
   }
 
-  framebuffer.save("surface_intersection");
+  framebuffer.save ("surface_intersection");
 
-  dray::stats::StatStore::write_ray_stats(config.m_camera.get_width(),
-                                          config.m_camera.get_height());
-
+  dray::stats::StatStore::write_ray_stats (config.m_camera.get_width (),
+                                           config.m_camera.get_height ());
 }
--- src/utilities/furnace/volume_rendering.cpp
+++ src/utilities/furnace/volume_rendering.cpp
@@ -9,64 +9,62 @@
 #include <random>
 
 
-int main(int argc, char* argv[])
+int main (int argc, char *argv[])
 {
   std::string config_file = "";
 
-  if(argc != 2)
+  if (argc != 2)
   {
-    std::cout<<"Missing configure file name\n";
-    exit(1);
+    std::cout << "Missing configure file name\n";
+    exit (1);
   }
 
   config_file = argv[1];
 
-  Config config(config_file);
-  config.load_data();
-  config.load_camera();
-  config.load_field();
+  Config config (config_file);
+  config.load_data ();
+  config.load_camera ();
+  config.load_field ();
 
   int trials = 5;
   // parse any custon info out of config
-  if(config.m_config.has_path("trials"))
+  if (config.m_config.has_path ("trials"))
   {
-    trials = config.m_config["trials"].to_int32();
+    trials = config.m_config["trials"].to_int32 ();
   }
 
-  dray::ColorTable color_table("Spectral");
-  color_table.add_alpha(0.f,  0.2f);
-  color_table.add_alpha(0.1f, 0.2f);
-  color_table.add_alpha(0.2f, 0.2f);
-  color_table.add_alpha(0.3f, 0.2f);
-  color_table.add_alpha(0.4f, 0.2f);
-  color_table.add_alpha(0.5f, 0.2f);
-  color_table.add_alpha(0.6f, 0.2f);
-  color_table.add_alpha(0.7f, 0.2f);
-  color_table.add_alpha(0.8f, 0.2f);
-  color_table.add_alpha(0.9f, 0.2f);
-  color_table.add_alpha(1.0f, 0.1f);
+  dray::ColorTable color_table ("Spectral");
+  color_table.add_alpha (0.f, 0.2f);
+  color_table.add_alpha (0.1f, 0.2f);
+  color_table.add_alpha (0.2f, 0.2f);
+  color_table.add_alpha (0.3f, 0.2f);
+  color_table.add_alpha (0.4f, 0.2f);
+  color_table.add_alpha (0.5f, 0.2f);
+  color_table.add_alpha (0.6f, 0.2f);
+  color_table.add_alpha (0.7f, 0.2f);
+  color_table.add_alpha (0.8f, 0.2f);
+  color_table.add_alpha (0.9f, 0.2f);
+  color_table.add_alpha (1.0f, 0.1f);
 
   dray::Array<dray::Ray> rays;
-  config.m_camera.create_rays(rays);
+  config.m_camera.create_rays (rays);
 
   dray::VolumeIntegrator integrator;
-  integrator.set_field(config.m_field);
-  integrator.set_color_table(color_table);
+  integrator.set_field (config.m_field);
+  integrator.set_color_table (color_table);
 
-  dray::Framebuffer framebuffer(config.m_camera.get_width(),
-                                config.m_camera.get_height());
+  dray::Framebuffer framebuffer (config.m_camera.get_width (),
+                                 config.m_camera.get_height ());
 
-  for(int i = 0; i < trials; ++i)
+  for (int i = 0; i < trials; ++i)
   {
-    framebuffer.clear();
-    config.m_camera.create_rays(rays);
-    integrator.execute(rays, config.m_dataset, framebuffer);
+    framebuffer.clear ();
+    config.m_camera.create_rays (rays);
+    integrator.execute (rays, config.m_dataset, framebuffer);
   }
 
-  framebuffer.save("volume");
+  framebuffer.save ("volume");
 
-  dray::stats::StatStore::write_ray_stats(config.m_camera.get_width(),
-                                          config.m_camera.get_height());
-
-
+  dray::stats::StatStore::write_ray_stats (config.m_camera.get_width (),
+                                           config.m_camera.get_height ());
 }
