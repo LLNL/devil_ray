d85da787ab3c290fd50a8fecd1cf0bba38019324
Ben Baral <bbaral@g.hmc.edu>
2020-02-09 17:15:46 -0800

    Implement all sorts properly

--- src/dray/sort.hpp
+++ src/dray/sort.hpp
@@ -1,23 +1,7 @@
 #ifndef DRAY_SORT_HPP
 #define DRAY_SORT_HPP
 
-#warning "included"
-
-//#include <algorithm>
-//#include <iostream>
-
 #include <dray/array_utils.hpp>
-#include <dray/bvh_utils.hpp>
-
-//// FIXME: remove this when header guard created
-////#define DRAY_OPENMP_ENABLED
-//
-//#if defined(DRAY_CUDA_ENABLED)
-//// NOTE: uses the cub installation that is bundled with RAJA
-//#include "cub/device/device_radix_sort.cuh"
-//
-//#elif defined(DRAY_OPENMP_ENABLED)
-//#include <omp.h>
 
 namespace dray {
 
