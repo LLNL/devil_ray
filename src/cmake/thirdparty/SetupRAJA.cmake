5e24fe4359ff5921692a8739b0c8acba4d1b9214
Matt Larsen <larsen30@llnl.gov>
2020-02-23 14:55:46 -0800

    pain

--- src/cmake/thirdparty/SetupRAJA.cmake
+++ src/cmake/thirdparty/SetupRAJA.cmake
@@ -3,11 +3,9 @@ if (NOT RAJA_DIR)
   message(FATAL_ERROR "Must specify 'RAJA_DIR'")
 endif()
 
-set(TEMP_RAJA_DIR ${RAJA_DIR})
+set(RAJA_BASE_DIR ${RAJA_DIR})
 find_dependency(RAJA REQUIRED
                NO_DEFAULT_PATH
                PATHS ${RAJA_DIR}/share/raja/cmake)
 
-# prevent RAJA from setting the raja dir
-set(RAJA_DIR ${TEMP_RAJA_DIR})
 message(STATUS "Found RAJA")
