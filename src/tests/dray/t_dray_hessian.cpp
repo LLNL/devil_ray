13b8f5b63493d4820768e6a6aa86348dbd5f053f
Masado Ishii <ishii3@llnl.gov>
2020-07-16 17:01:06 -0700

    Flesh out tests for hessian zero and constant

--- src/tests/dray/t_dray_hessian.cpp
+++ src/tests/dray/t_dray_hessian.cpp
@@ -13,6 +13,7 @@
 #include <dray/Element/elem_ops.hpp>
 
 #include <dray/GridFunction/field.hpp>
+#include <dray/GridFunction/device_field.hpp>
 
 #include <iostream>
 
@@ -33,11 +34,32 @@ TEST(dray_hessian, dray_zero_hessian)
 
   // Add a constant field.
   const dray::Vec<Float, 1> constant = {{5.0f}};
-  const std::string name = "VeryConstant";
+  const std::string field_name = "VeryConstant";
   for (dray::DataSet &ds : collxn.domains())
     ds.add_field(std::make_shared<dray::Field<ScalarFElemT>>(
-          dray::Field<ScalarFElemT>::uniform_field(ds.topology()->cells(), constant, name)));
+          dray::Field<ScalarFElemT>::uniform_field(ds.topology()->cells(), constant, field_name)));
 
+  dray::DataSet &data_set = *collxn.domains().begin();
+
+  // We know what type the field is because we just made it.
+  dray::Field<ScalarFElemT> &field = * (dray::Field<ScalarFElemT> *)(data_set.field(field_name));
+  dray::DeviceField<ScalarFElemT> dfield(field);
+
+  // Note: DeviceField should only be used on the device. This code will not work on cuda.
+  {
+    dray::Matrix<dray::Vec<Float, 1>, 3, 3> H_elem0;
+
+    std::cerr << "Warning: If attempting to run with cuda, expect a segfault.\n";
+
+    const dray::Vec<Float, 3> ref_coord = {{0.1, 0.3, 0.6}};
+    dray::eops::eval_hessian(dray::ShapeHex(),
+                             dfield.get_order_policy(),
+                             (dray::ReadDofPtr<dray::Vec<Float, 1>>) dfield.get_elem(0).read_dof_ptr(),
+                             ref_coord,
+                             H_elem0);
+
+    std::cout << "H_elem0:  \n" << H_elem0 << "\n";
+  }
 }
 
 
@@ -49,14 +71,38 @@ TEST(dray_hessian, dray_constant_hessian)
   const dray::Vec<Float, 3> origin = {{0.0f, 0.0f, 0.0f}};
   const dray::Vec<Float, 3> radius = {{1.0f, 1.0f, 1.0f}};
   const dray::Vec<Float, 3> range_radius = {{1.0f, 1.0f, -1.0f}};
-  const dray::Vec<Float, 3> range_radius_aux = {{1.0f, 1.0f, -1.0f}};
+
+  const std::string field_name = "perfection";
 
   dray::Collection collxn =
       dray::SynthesizeAffineRadial(extents, origin, radius)
-      .equip("perfection", range_radius)
-      .equip("aux", range_radius_aux)
+      .equip(field_name, range_radius)
       .synthesize();
 
+  dray::DataSet &data_set = *collxn.domains().begin();
+
+  using ScalarFElemT = dray::Element<3, 1, dray::Tensor, 2>;
+
+  // I know this is the type of the field, just accept it.
+  dray::Field<ScalarFElemT> &field = * (dray::Field<ScalarFElemT> *)(data_set.field(field_name));
+  dray::DeviceField<ScalarFElemT> dfield(field);
+
+  // Note: DeviceField should only be used on the device. This code will not work on cuda.
+  {
+    dray::Matrix<dray::Vec<Float, 1>, 3, 3> H_elem0;
+
+    std::cerr << "Warning: If attempting to run with cuda, expect a segfault.\n";
+
+    const dray::Vec<Float, 3> ref_coord = {{0.1, 0.3, 0.6}};
+    dray::eops::eval_hessian(dray::ShapeHex(),
+                             /*dfield.get_order_policy(),*/
+                                 dray::OrderPolicy<dray::General>{2},  // Interim before specializations implemented.
+                             (dray::ReadDofPtr<dray::Vec<Float, 1>>) dfield.get_elem(0).read_dof_ptr(),
+                             ref_coord,
+                             H_elem0);
+
+    std::cout << "H_elem0:  \n" << H_elem0 << "\n";
+  }
 }
 
 
