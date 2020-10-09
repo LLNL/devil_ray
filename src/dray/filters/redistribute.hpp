11e9fe8a8d269ce2c6ce6d8f035c34369b8e6f87
Matt Larsen <larsen30@llnl.gov>
2020-10-09 08:51:37 -0700

    initial sending

--- src/dray/filters/redistribute.hpp
+++ src/dray/filters/redistribute.hpp
@@ -6,12 +6,39 @@
 namespace dray
 {
 
+
+
 class Redistribute
 {
 protected:
+
+  struct CommInfo
+  {
+    int32 m_dest_rank;
+    int32 m_domain_id;
+    int32 m_src_rank;
+    int32 m_src_idx; // local domain to send (only used by sender)
+    static bool compare(const CommInfo &a, const CommInfo &b)
+    {
+      return a.m_domain_id < b.m_domain_id;
+    }
+  };
+
+  std::vector<CommInfo> m_comm_info;
+
 public:
   Redistribute();
-  Collection execute(Collection &collection);
+
+  Collection execute(Collection &collection,
+                     const std::vector<int32> &src_list,
+                     const std::vector<int32> &dest_list);
+protected:
+
+  void build_schedule(Collection &collection,
+                      const std::vector<int32> &src_list,
+                      const std::vector<int32> &dest_list);
+
+  void send_recv_metadata(Collection &collection);
 };
 
 };//namespace dray
