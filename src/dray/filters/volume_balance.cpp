d4997587cbb2b9bd0904059d4df0ed42878ff680
larsen30@llnl.gov <larsen30@rzgenie7.llnl.gov>
2020-10-24 18:56:51 -0700

    some cleanup

--- src/dray/filters/volume_balance.cpp
+++ src/dray/filters/volume_balance.cpp
@@ -85,6 +85,29 @@ void aabb_cells(Mesh<MeshElement> &mesh,
   DRAY_LOG_CLOSE();
 }
 
+template<typename MeshElement>
+void volume_sum(Mesh<MeshElement> &mesh,
+                float32 &sum)
+{
+  DRAY_LOG_OPEN("volume_sum");
+
+  const int32 num_elems = mesh.get_num_elem();
+  DeviceMesh<MeshElement> device_mesh(mesh);
+  RAJA::ReduceSum<reduce_policy, float32> vsum(0.);
+
+  RAJA::forall<for_policy>(RAJA::RangeSegment(0, num_elems), [=] DRAY_LAMBDA (int32 i)
+  {
+    int32 mask_val = 0;
+    MeshElement element = device_mesh.get_elem(i);
+    AABB<3> elem_bounds;
+    element.get_bounds(elem_bounds);
+    vsum += elem_bounds.volume();
+  });
+
+  sum = vsum.get();
+  DRAY_LOG_CLOSE();
+}
+
 struct MaskFunctor
 {
   Array<int32> m_mask;
@@ -121,11 +144,24 @@ struct AABBFunctor
   }
 };
 
+struct VolumeSumFunctor
+{
+  float32 m_sum;
+  VolumeSumFunctor()
+  {
+  }
 
-void split(const DomainTask &task,
+  template<typename TopologyType>
+  void operator()(TopologyType &topo)
+  {
+    volume_sum(topo.mesh(), m_sum);
+  }
+};
+
+
+void split(std::vector<float32> &pieces,
            DataSet &dataset,
-           Collection &col,
-           std::vector<int32> &dests)
+           Collection &col)
 {
   DRAY_LOG_OPEN("split");
 
@@ -137,21 +173,15 @@ void split(const DomainTask &task,
   DRAY_LOG_ENTRY("box_volume", bounds.volume());
   DRAY_LOG_ENTRY("box_min", bounds.m_ranges[max_comp].min());
 
-  float32 total = task.m_amount;
-
-  std::vector<float32> pieces;
-  pieces.resize(task.m_splits.size() + 1);
-  pieces[0] = task.m_amount;
-  DRAY_LOG_ENTRY("split_0", task.m_amount);
-
-  for(int32 i = 0; i < task.m_splits.size(); ++i)
-  {
-    pieces[i+1] = task.m_splits[i].first;
-    total += task.m_splits[i].first;
-    DRAY_LOG_ENTRY("split", task.m_splits[i].first);
-  }
 
   const int32 num_pieces = pieces.size();
+  float32 total = 0;
+  for(int32 i = 0; i < num_pieces; ++i)
+  {
+    total += pieces[i];
+    DRAY_LOG_ENTRY("split", pieces[i]);
+  }
+
   std::vector<float32> normalized_volume;
   normalized_volume.resize(num_pieces);
 
@@ -161,6 +191,12 @@ void split(const DomainTask &task,
     pieces[i] /= total;
     normalized_volume[i] = pieces[i];
     DRAY_LOG_ENTRY("normalized_length", pieces[i]);
+  }
+  // prefix sum 
+  for(int32 i = 1; i < num_pieces; ++i)
+  {
+    normalized_volume[i] += normalized_volume[i-1];
+    DRAY_LOG_ENTRY("normalized_sum_length", normalized_volume[i]);
   }
 
   // scale
@@ -186,31 +222,33 @@ void split(const DomainTask &task,
   {
     aabb_tot_vol += aabbs_ptr[i].volume();
   }
+
   DRAY_LOG_ENTRY("aabbs_vol", aabb_tot_vol);
+  DRAY_LOG_ENTRY("aabbs_size", aabbs.size());
 
   std::vector<int32> divs;
   divs.resize(num_pieces);
   float32 curr_vol = 0;
   float32 piece_idx = 0;
 
-  for(int32 i = 0; i < aabbs.size() && piece_idx < num_pieces; ++i)
+  for(int32 i = 0; i < aabbs.size(); ++i)
   {
     curr_vol += aabbs_ptr[i].volume() / aabb_tot_vol;
     if(curr_vol >= normalized_volume[piece_idx])
     {
       divs[piece_idx] = i;
       DRAY_LOG_ENTRY("div_normal", curr_vol);
+      DRAY_LOG_ENTRY("div_normal_idx", i);
       piece_idx++;
-      curr_vol = 0;
     }
   }
+
   divs[num_pieces - 1] = aabbs.size() - 1;
 
   for(int32 i = 0; i < num_pieces; ++i)
   {
     DRAY_LOG_ENTRY("div", divs[i]);;
   }
-
 
   std::vector<float32> ranges;
   ranges.resize(num_pieces+1);
@@ -248,14 +286,6 @@ void split(const DomainTask &task,
     if(piece.topology()->cells() > 0)
     {
       col.add_domain(piece);
-      if(i == 0)
-      {
-        dests.push_back(dray::mpi_rank());
-      }
-      else
-      {
-        dests.push_back(task.m_splits[i-1].second);
-      }
     }
   }
   DRAY_LOG_CLOSE();
@@ -279,12 +309,21 @@ VolumeBalance::schedule_blocks2(std::vector<float32> &rank_volumes,
   const int32 size = rank_volumes.size();
   const int32 global_size = global_volumes.size();
 
+  std::vector<int32> random;
+  random.resize(global_size);
+  for(int32 i = 0; i < global_size; ++i)
+  {
+    random[i] = i;
+  }
+
+  std::random_shuffle(random.begin(), random.end());
+
   std::vector<float32> prefix_sum;
   prefix_sum.resize(global_size);
-  prefix_sum[0] = global_volumes[0];
+  prefix_sum[random[0]] = global_volumes[random[0]];
   for(int32 i = 1; i < global_size; ++i)
   {
-    prefix_sum[i] = global_volumes[i] + prefix_sum[i-1];
+    prefix_sum[random[i]] = global_volumes[random[i]] + prefix_sum[random[i-1]];
   }
 
   // init everthing to stay in place
@@ -309,7 +348,6 @@ VolumeBalance::schedule_blocks2(std::vector<float32> &rank_volumes,
 
   // target
   const float32 ave = sum / float32(size);
-  DRAY_LOG_ENTRY("average", ave);
 
   int32 pos = 0;
   for(int32 i = 0; i < size - 1; ++i)
@@ -323,58 +361,44 @@ VolumeBalance::schedule_blocks2(std::vector<float32> &rank_volumes,
     float32 target = ave * float32(i+1); 
     while(idx < global_size - 1)
     {
-      float32 vol = prefix_sum[idx+1];
-      DRAY_INFO("target "<<target<<" next "<<vol);
+      float32 vol = prefix_sum[random[idx+1]];
       if(vol > target)
       {
         break;
       }
       idx++;
-      rank_volumes[i] += global_volumes[idx];
-      DRAY_INFO("idx "<<idx<<" to rank "<<i);
+      rank_volumes[i] += global_volumes[random[idx]];
     }
 
-    float32 d1 = std::abs(target - prefix_sum[idx]);
-    float32 d2 = std::abs(target - prefix_sum[idx+1]);
-    DRAY_INFO("d1 "<<d1<<" d2 "<<d2);
+    float32 d1 = std::abs(target - prefix_sum[random[idx]]);
+    float32 d2 = std::abs(target - prefix_sum[random[idx+1]]);
 
     if(d2 < d1)
     {
       idx++;
-      rank_volumes[i] += global_volumes[idx];
-      DRAY_INFO("idx "<<idx<<" to rank "<<i);
+      rank_volumes[i] += global_volumes[random[idx]];
     }
-    DRAY_INFO("rank "<<i<<" volume "<<rank_volumes[i]<<" ave "<<ave);
-    DRAY_INFO("rank "<<i<<" start "<<pos<<" end "<<idx);
+
     for(int32 domain = pos; domain <= idx; ++domain)
     {
-      dest_list[domain] = i;
+      dest_list[random[domain]] = i;
     }
     pos = idx + 1;
   } 
 
   for(int32 i = pos; i < global_size; ++i)
   {
-    dest_list[i] = size - 1;
-    rank_volumes[size -1] += global_volumes[i];
+    dest_list[random[i]] = size - 1;
+    rank_volumes[size -1] += global_volumes[random[i]];
   }
 
 
   float32 max_after = 0;
-  Logger::get_instance()->get_stream() <<"Rank volumes\n";
   for(int32 i = 0; i < size; ++i)
   {
-    Logger::get_instance()->get_stream() <<rank_volumes[i]<<" ";
     max_after = std::max(max_after,rank_volumes[i]);
   }
-  Logger::get_instance()->get_stream() <<"\n";
 
-  Logger::get_instance()->get_stream() <<"Destinations\n";
-  for(int32 i = 0; i < global_size; ++i)
-  {
-    Logger::get_instance()->get_stream() <<dest_list[i]<<" ";
-  }
-  Logger::get_instance()->get_stream() <<"\n";
   return max_after / max_val;
 }
 
@@ -435,7 +459,7 @@ VolumeBalance::schedule_blocks(std::vector<float32> &rank_volumes,
   float32 eps = rank_volumes[idx[giver]] * 1e-3;
   float32 max_val = rank_volumes[idx[giver]];
 
-  float32 target = ave;
+  float32 target = ave * 1.1;
   conduit::Node rounds;
   while(giver > taker)
   {
@@ -504,199 +528,6 @@ VolumeBalance::schedule_blocks(std::vector<float32> &rank_volumes,
   return max_after / max_val;
 }
 
-float32
-VolumeBalance::perfect_splitting(std::vector<RankTasks> &distribution)
-{
-  const int32 size = distribution.size();
-
-  std::vector<int32> idx(size);
-  std::iota(idx.begin(), idx.end(), 0);
-  stable_sort(idx.begin(), idx.end(),
-       [&distribution](int32 i1, int32 i2)
-       {
-         return distribution[i1].amount() < distribution[i2].amount();
-       });
-
-  float32 sum = 0;
-  for(int32 i = 0; i < size; ++i)
-  {
-    sum += distribution[i].amount();
-  }
-  const float32 ave = sum / float32(size);
-
-  int32 giver = size - 1;
-  int32 taker = 0;
-  float32 eps = distribution[idx[giver]].amount() * 1e-3;
-  float32 max_val = distribution[idx[giver]].amount();
-  //std::cout<<"Taker "<<taker<<"\n";
-  while(giver > taker)
-  {
-    //std::cout<<"Giver "<<giver<<" taker "<<taker<<"\n";
-    int32 giver_idx = idx[giver];
-    int32 taker_idx = idx[taker];
-    float32 giver_work = distribution[giver_idx].amount();
-    if(giver_work < ave)
-    {
-      break;
-    }
-
-    float32 giver_dist = giver_work - ave;
-
-    float32 taker_dist = ave - distribution[taker_idx].amount();
-    float32 give_amount = 0;
-    if(taker_dist > giver_dist)
-    {
-      give_amount = giver_dist;
-    }
-    else
-    {
-      give_amount = taker_dist;
-    }
-
-    give_amount = distribution[giver_idx].split_biggest(give_amount,taker_idx);
-    distribution[taker_idx].add(give_amount);
-
-    // does giver have more?
-    if(distribution[giver_idx].amount() <= ave + eps)
-    {
-      giver--;
-    }
-    // can taker take more?
-    if(distribution[taker_idx].amount() >= ave - eps)
-    {
-      taker++;
-    }
-  }
-  //std::cout<<"giver "<<giver<<" taker "<<taker<<"\n";
-
-  float32 max_after = 0;
-  for(int32 i = 0; i < size; ++i)
-  {
-    max_after = std::max(distribution[i].amount(), max_after);
-  }
-
-  if(dray::mpi_rank() == 0)
-  {
-    std::ofstream load_file;
-    load_file.open ("perfect_splits.txt");
-    for(auto &l : distribution)
-    {
-      load_file << l.amount() <<"\n";
-    }
-    load_file.close();
-  }
-  return max_after / max_val;
-}
-
-Collection
-VolumeBalance::execute(Collection &collection, Camera &camera)
-{
-  DRAY_LOG_OPEN("volume_balance");
-  Collection res;
-
-  const int32 local_doms = collection.local_size();
-  std::vector<float32> local_volumes;
-  local_volumes.resize(local_doms);
-
-  float32 total_volume = 0;
-  for(int32 i = 0; i < collection.local_size(); ++i)
-  {
-    DataSet dataset = collection.domain(i);
-    AABB<3> bounds = dataset.topology()->bounds();
-    float32 pixels = static_cast<float32>(camera.subset_size(bounds));
-    local_volumes[i] = bounds.volume() * pixels;
-    total_volume += local_volumes[i];
-  }
-
-  DRAY_LOG_ENTRY("local_volume", total_volume);
-
-
-#ifdef DRAY_MPI_ENABLED
-  MPI_Comm mpi_comm = MPI_Comm_f2c(dray::mpi_comm());
-  const int32 comm_size = dray::mpi_size();
-  const int32 rank = dray::mpi_rank();
-  const int32 global_size = collection.size();
-
-  std::vector<int32> global_counts;
-  global_counts.resize(comm_size);
-  MPI_Allgather(&local_doms, 1, MPI_INT, &global_counts[0], 1, MPI_INT, mpi_comm);
-
-  std::vector<int32> global_offsets;
-  global_offsets.resize(comm_size);
-  global_offsets[0] = 0;
-
-  for(int i = 1; i < comm_size; ++i)
-  {
-    global_offsets[i] = global_offsets[i-1] + global_counts[i-1];
-  }
-
-  std::vector<float32> global_volumes;
-  global_volumes.resize(global_size);
-
-  std::vector<float32> rank_volumes;
-  rank_volumes.resize(comm_size);
-
-  MPI_Allgather(&total_volume,1, MPI_FLOAT,&rank_volumes[0],1,MPI_FLOAT,mpi_comm);
-
-  MPI_Allgatherv(&local_volumes[0],
-                 local_doms,
-                 MPI_FLOAT,
-                 &global_volumes[0],
-                 &global_counts[0],
-                 &global_offsets[0],
-                 MPI_FLOAT,
-                 mpi_comm);
-
-
-  std::vector<RankTasks> distribution;
-  distribution.resize(comm_size);
-  for(int32 i = 0; i < comm_size; ++i)
-  {
-    distribution[i].m_rank = i;
-    for(int32 t = 0; t < global_counts[i]; ++t)
-    {
-      const int32 offset = global_offsets[i];
-      distribution[i].add_task(global_volumes[offset + t]);
-    }
-  }
-  float32 ratio = perfect_splitting(distribution);
-  if(rank == 0)
-  {
-    std::cout<<"Ratio "<<ratio<<"\n";
-  }
-
-  std::vector<int32> src_list;
-  std::vector<int32> dest_list;
-  Collection chopped = chopper(distribution, collection, src_list, dest_list);
-  DRAY_LOG_ENTRY("chopped_local_domains", chopped.local_size());
-  //if(rank == 0)
-  //{
-  //  for(int i = 0; i < src_list.size(); ++i)
-  //  {
-  //    std::cout<<"src "<<src_list[i]<<" -> "<<dest_list[i]<<"\n";
-  //  }
-  //}
-
-  //map(distribution, src_list, dest_list);
-
-  Redistribute redist;
-  res = redist.execute(chopped, src_list, dest_list);
-  DRAY_LOG_ENTRY("result_local_domains", res.local_size());
-#endif
-
-  total_volume = 0;
-  for(int32 i = 0; i < res.local_size(); ++i)
-  {
-    DataSet dataset = res.domain(i);
-    AABB<3> bounds = dataset.topology()->bounds();
-    float32 pixels = static_cast<float32>(camera.subset_size(bounds));
-    total_volume += bounds.volume() * pixels;
-  }
-  DRAY_LOG_ENTRY("result_local_volume", total_volume);
-  DRAY_LOG_CLOSE();
-  return res;
-}
-
 Collection
 VolumeBalance::chopper(float32 piece_size,
                        std::vector<float32> &sizes,
@@ -705,17 +536,28 @@ VolumeBalance::chopper(float32 piece_size,
   Collection res;
   for(int32 i = 0; i < collection.local_size(); ++i)
   {
-    DomainTask task;
-    task.m_amount = sizes[i];
+    float32 psize = sizes[i];
     DataSet dataset = collection.domain(i);
-    // we are not using dests
-    while(task.m_amount > piece_size)
+    int32 num_pieces = std::ceil(psize / piece_size);
+    if(num_pieces > 1)
     {
-      task.chunk(piece_size, -1);
+      std::vector<float32> pieces; 
+      for(int32 p = 0; p < num_pieces; ++p)
+      {
+        pieces.push_back(piece_size);
+      }
+      float32 rem = psize - num_pieces * piece_size;
+      if(rem > psize * 1.e-2 )
+      {
+        pieces.push_back(rem); 
+      }
+      detail::split(pieces, dataset, res);
+    }
+    else
+    {
+      res.add_domain(dataset);
     }
 
-    std::vector<int32> dests_not_used;
-    detail::split(task, dataset, res, dests_not_used);
   }
 
   return res;
@@ -781,16 +623,27 @@ VolumeBalance::volumes(Collection &collection,
   {
     DataSet dataset = collection.domain(i);
     AABB<3> bounds = dataset.topology()->bounds();
+
+    float32 sample_distance = 0.0433;
+    float32 samples = bounds.m_ranges[bounds.max_dim()].length() / sample_distance;
+
+    //float32 volume = bounds.volume();
+    detail::VolumeSumFunctor vfunc;
+    dispatch(dataset.topology(), vfunc);
+    //float32 volume = vfunc.m_sum;
+    float32 volume = samples;
+
     float32 pixels = static_cast<float32>(camera.subset_size(bounds));
     float32 normalized_pixels = pixels / float32(camera.get_width() + camera.get_height());
-    volumes[i] = bounds.volume() * normalized_pixels;
+    //volumes[i] = volume * normalized_pixels;
+    volumes[i] = volume * pixels;
     total_volume += volumes[i];
   }
   return total_volume;
 }
 
 Collection
-VolumeBalance::execute2(Collection &collection, Camera &camera)
+VolumeBalance::execute(Collection &collection, Camera &camera)
 {
   DRAY_LOG_OPEN("volume_balance");
   Collection res;
@@ -814,14 +667,11 @@ VolumeBalance::execute2(Collection &collection, Camera &camera)
 
   float32 global_ave = global_volume / float32(comm_size);
   int32 pieces = (int32) std::ceil(total_volume / global_ave);
-  float32 piece_size = total_volume / float32(pieces);
+  float32 piece_size = total_volume / float32(1+pieces);
 
   DRAY_LOG_ENTRY("piece_size", piece_size);
 
-
   Collection pre_chopped = chopper(piece_size, local_volumes, collection);
-
-  
 
   const int32 chopped_size = pre_chopped.size();
   std::vector<float32> rank_volumes;
@@ -849,134 +699,28 @@ VolumeBalance::execute2(Collection &collection, Camera &camera)
   dest_list.resize(chopped_size);
 
   float32 ratio  = schedule_blocks2(rank_volumes,
-                                  global_counts,
-                                  global_offsets,
-                                  global_volumes,
-                                  src_list,
-                                  dest_list);
+                                   global_counts,
+                                   global_offsets,
+                                   global_volumes,
+                                   src_list,
+                                   dest_list);
 
-  if(rank == 0)
-  {
-    std::cout<<"Ratio "<<ratio<<"\n";
-    std::cout<<"chopped size "<<chopped_size<<" src size "<<src_list.size()<<"\n";
-  }
+  DRAY_LOG_ENTRY("ratio",ratio);
 
   Redistribute redist;
   res = redist.execute(pre_chopped, src_list, dest_list);
   DRAY_LOG_ENTRY("result_local_domains", res.local_size());
 #endif
 
-  total_volume = 0;
+  std::vector<float32> res_local;
+  total_volume = volumes(res, camera, res_local);
   for(int32 i = 0; i < res.local_size(); ++i)
   {
-    DataSet dataset = res.domain(i);
-    AABB<3> bounds = dataset.topology()->bounds();
-    float32 pixels = static_cast<float32>(camera.subset_size(bounds));
-    float32 normalized_pixels = pixels / float32(camera.get_width() + camera.get_height());
-    total_volume += bounds.volume() * normalized_pixels;
+    DRAY_LOG_ENTRY("result_domain_volume", res_local[i]);
   }
   DRAY_LOG_ENTRY("result_local_volume", total_volume);
   DRAY_LOG_CLOSE();
   return res;
-}
-
-Collection
-VolumeBalance::chopper(const std::vector<RankTasks> &distribution,
-                       Collection &collection,
-                       std::vector<int32> &src_list,
-                       std::vector<int32> &dest_list)
-{
-  Collection res;
-  const int32 rank = dray::mpi_rank();
-  const RankTasks &tasks = distribution[rank];
-
-  const int32 num_tasks = tasks.m_tasks.size();
-  std::vector<int32> local_dests;
-  for(int32 i = 0; i < num_tasks; ++i)
-  {
-    DataSet dataset = collection.domain(i);
-    const DomainTask &task = tasks.m_tasks[i];
-    if(task.m_splits.size() > 0)
-    {
-      detail::split(task, dataset, res, local_dests);
-    }
-    else
-    {
-      // no splits just pass through the data
-      res.add_domain(dataset);
-      local_dests.push_back(rank);
-    }
-  }
-#ifdef DRAY_MPI_ENABLED
-  int32 local_doms = res.local_size();
-
-  if(local_dests.size() != local_doms)
-  {
-    DRAY_ERROR("dests and doms mismatch");
-  }
-
-  MPI_Comm mpi_comm = MPI_Comm_f2c(dray::mpi_comm());
-  const int32 comm_size = dray::mpi_size();
-  const int32 global_size = res.size();
-
-  std::vector<int32> global_counts;
-  global_counts.resize(comm_size);
-  MPI_Allgather(&local_doms, 1, MPI_INT, &global_counts[0], 1, MPI_INT, mpi_comm);
-
-  std::vector<int32> global_offsets;
-  global_offsets.resize(comm_size);
-  global_offsets[0] = 0;
-
-  for(int i = 1; i < comm_size; ++i)
-  {
-    global_offsets[i] = global_offsets[i-1] + global_counts[i-1];
-  }
-
-  dest_list.resize(global_size);
-
-  MPI_Allgatherv(&local_dests[0],
-                 local_doms,
-                 MPI_INT,
-                 &dest_list[0],
-                 &global_counts[0],
-                 &global_offsets[0],
-                 MPI_INT,
-                 mpi_comm);
-  src_list.resize(global_size);
-  for(int32 i = 0; i < comm_size; ++i)
-  {
-    const int32 start = global_offsets[i];
-    for(int32 c = 0; c < global_counts[i]; ++c)
-    {
-      src_list[start + c] = i;
-    }
-  }
-#endif
-  return res;
-}
-
-void
-VolumeBalance::map(const std::vector<RankTasks> &distribution,
-                   std::vector<int32> &src_list,
-                   std::vector<int32> &dest_list)
-{
-  const int32 size = distribution.size();
-  for(int32 i = 0; i < size; ++i)
-  {
-    const RankTasks &tasks = distribution[i];
-    const int32 num_tasks = tasks.m_tasks.size();
-    for(int32 task_idx = 0; task_idx < num_tasks; ++task_idx)
-    {
-      // one part stays on this rank
-      src_list.push_back(i);
-      dest_list.push_back(i);
-      for(auto &split : tasks.m_tasks[task_idx].m_splits)
-      {
-        src_list.push_back(i);
-        dest_list.push_back(split.second);
-      }
-    }
-  }
 }
 
 }//namespace dray
--- src/dray/filters/volume_balance.hpp
+++ src/dray/filters/volume_balance.hpp
@@ -7,94 +7,12 @@
 namespace dray
 {
 
-struct DomainTask
-{
-  float32 m_amount;
-  // what was chopped off and where is it going
-  std::vector<std::pair<float32, int32>> m_splits;
-  void chunk(float32 amount, int32 dest_rank)
-  {
-    std::pair<float32, int32> split;
-    split.first = amount;
-    split.second = dest_rank;
-    m_amount -= amount;
-    m_splits.push_back(split);
-
-    if(m_amount <= 0)
-    {
-      std::cout<<"task that was split now <=0\n";
-    }
-  }
-};
-
-struct RankTasks
-{
-  std::vector<DomainTask> m_tasks;
-  float32 m_total_amount;
-  int32 m_rank;
-
-  void add_task(const float32 amount)
-  {
-    DomainTask task;
-    task.m_amount = amount;
-    m_total_amount += amount;
-    m_tasks.push_back(task);
-  }
-
-  float32 amount() const
-  {
-    return m_total_amount;
-  }
-
-  void add(const float32 amount)
-  {
-    m_total_amount += amount;
-  }
-
-  int32 biggest()
-  {
-    int32 max_index = -1;
-    float32 max_amount = -1.f;
-
-    const int32 size = m_tasks.size();
-    for(int32 i = 0; i < size; ++i)
-    {
-      if(m_tasks[i].m_amount > max_amount)
-      {
-        max_index = i;
-        max_amount = m_tasks[i].m_amount;
-      }
-    }
-    return max_index;
-  }
-
-  float32 split_biggest(const float32 amount, const int32 dest_rank)
-  {
-    int32 idx = biggest();
-    if(idx == -1) std::cout<<"bad\n";
-
-    m_tasks[idx].chunk(amount, dest_rank);
-
-    m_total_amount -= amount;
-
-    if(m_total_amount <= 0)
-    {
-      std::cout<<"Rank task total now <=0\n";
-    }
-    // future: this could return
-    return amount;
-  }
-};
-
 class VolumeBalance
 {
 protected:
 public:
   VolumeBalance();
   Collection execute(Collection &collection, Camera &camera);
-  Collection execute2(Collection &collection, Camera &camera);
-
-  float32 perfect_splitting(std::vector<RankTasks> &distribution);
 
   float32 schedule_blocks(std::vector<float32> &rank_volumes,
                           std::vector<int32> &global_counts,
@@ -109,11 +27,6 @@ public:
                            std::vector<float32> &global_volumes,
                            std::vector<int32> &src_list,
                            std::vector<int32> &dest_list);
-
-  Collection chopper(const std::vector<RankTasks> &distribution,
-                     Collection &collection,
-                     std::vector<int32> &src_list,
-                     std::vector<int32> &dest_list);
 
   Collection chopper(float32 piece_size,
                      std::vector<float32> &sizes,
@@ -130,9 +43,6 @@ public:
                   Camera &camera,
                   std::vector<float32> &volumes);
 
-  void map(const std::vector<RankTasks> &distribution,
-           std::vector<int32> &src_list,
-           std::vector<int32> &dest_list);
 };
 
 };//namespace dray
