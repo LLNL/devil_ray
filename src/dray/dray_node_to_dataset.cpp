2096d6c434e31255c44d64b9c61b63632abce89b
Matt Larsen <larsen30@llnl.gov>
2020-10-26 10:12:43 -0700

    missed another

--- src/dray/dray_node_to_dataset.cpp
+++ src/dray/dray_node_to_dataset.cpp
@@ -23,9 +23,9 @@ std::vector<std::string> split (std::string s, std::string delimiter)
 {
   size_t pos_start = 0, pos_end, delim_len = delimiter.length();
   std::string token;
-  std::vector<string> res;
+  std::vector<std::string> res;
 
-  while ((pos_end = s.find (delimiter, pos_start)) != string::npos)
+  while ((pos_end = s.find (delimiter, pos_start)) != std::string::npos)
   {
     token = s.substr (pos_start, pos_end - pos_start);
     pos_start = pos_end + delim_len;
