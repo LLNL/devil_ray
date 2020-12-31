6453daa5bf0ad6916f525781c99f52055a24b560
Masado Ishii <ishii3@llnl.gov>
2020-12-30 16:36:35 -0800

    Copy SphericalHarmonics from the test into the filter. std::vector -> new[].

--- src/dray/filters/first_scatter.cpp
+++ src/dray/filters/first_scatter.cpp
@@ -313,80 +313,8 @@ Array<Float> integrate_moments(Array<Vec<Float,3>> &destinations,
                                Array<Vec<Float,3>> &ray_sources,
                                Array<int32> &source_cells,
                                Float _cell_volume,
-                               LowOrderField *emission)
-{
-  if (legendre_order > 0)
-  {
-    throw std::logic_error("integrate_moments() needs SphericalHarmonics "
-                           "to handle legendre_order > 0.");
-  }
+                               LowOrderField *emission);
 
-  const int32 ncomp = path_lengths.ncomp();
-  const int32 num_moments = (legendre_order + 1) * (legendre_order + 1);
-  const int32 num_destinations = destinations.size();
-  const int32 num_sources = ray_sources.size();
-
-  Array<Float> destination_moments;
-  destination_moments.resize(num_destinations * num_moments, ncomp);
-
-  const Float cell_volume = _cell_volume;
-
-  ConstDeviceArray<Vec<Float, 3>> destinations_dev(destinations);
-  ConstDeviceArray<Vec<Float, 3>> ray_sources_dev(ray_sources);
-  ConstDeviceArray<Float> path_lengths_dev(path_lengths);
-  ConstDeviceArray<int32> source_cells_dev(source_cells);
-  ConstDeviceArray<Float> emission_dev(emission->values());
-
-  NonConstDeviceArray<Float> destination_moments_dev(destination_moments);
-
-  RAJA::forall<for_policy> (RAJA::RangeSegment(0, num_destinations),
-      [=] DRAY_LAMBDA (int32 dest)
-  {
-    // Clear output.
-    for (int32 nm = 0; nm < num_moments; ++nm)
-      for (int32 component = 0; component < ncomp; ++component)
-        destination_moments_dev.get_item(num_moments * dest + nm, component) = 0.0f;
-
-    // For each source
-    //   For each component
-    //     For each moment
-    //       Multiply-and-accumulate source term with spherical harmonic.
-    const Vec<Float, 3> dest_pos = destinations_dev.get_item(dest);
-    for (int32 source = 0; source < num_sources; ++source)
-    {
-      const Vec<Float, 3> omega = (dest_pos - ray_sources_dev.get_item(source));
-      const Vec<Float, 3> omega_hat = omega.normalized();
-      const Float rcp_mag2 = rcp_safe(omega.magnitude2());
-      // Really should use volume-average (over source cell) of rcp_mag2.
-
-      // TODO spherical harmonics object evaluate all at omega_hat
-      //sph_all
-
-      const int32 source_idx = source_cells_dev.get_item(source);
-
-      for (int32 component = 0; component < ncomp; ++component)
-      {
-        const Float dEmission_dV = emission_dev.get_item(source_idx, component);
-        const Float source_dL_dOmega
-          = dEmission_dV * cell_volume * rcp_mag2;
-
-        const Float transmitted = path_lengths_dev.get_item(
-            num_sources * dest + source, component);
-
-        const Float trans_source = transmitted * source_dL_dOmega;
-
-        for (int32 nm = 0; nm < num_moments; ++nm)
-        {
-          const Float spherical_harmonic = 1.0f;  // TODO lookup from sph_all
-          destination_moments_dev.get_item(num_moments * dest + nm, component)
-              += spherical_harmonic * trans_source;
-        }//moments
-      }//components
-    }//sources
-  });//destinations
-
-  return destination_moments;
-}
 
 
 void FirstScatter::execute(DataSet &data_set)
@@ -430,7 +358,7 @@ void FirstScatter::execute(DataSet &data_set)
       DRAY_ERROR("Emission field must be associated with elements");
     }
 
-    const int32 legendre_order = 0;  // TODO get legendre_order from dataset?
+    const int32 legendre_order = 3;  // TODO get legendre_order from dataset?
 
     Array<int32> source_cells;
     Array<Vec<Float,3>> ray_sources = detail::cell_centers_nonzero(*uni_topo, emission, source_cells);
@@ -451,6 +379,9 @@ void FirstScatter::execute(DataSet &data_set)
 
     //TODO return destination_moments
 
+    std::cout << "destinations.size() == " << destinations.size() << "\n";
+    std::cout << "legendre_order == " << legendre_order << "\n";
+    std::cout << "num_moments == " << (legendre_order+1)*(legendre_order+1) << "\n";
     std::cout << "destination_moments.size() == " << destination_moments.size() << "\n";
   }
   else
@@ -468,6 +399,337 @@ void FirstScatter::emission_field(const std::string field_name)
 {
   m_emission_field = field_name;
 }
+
+
+
+class SphericalHarmonics
+{
+  public:
+    DRAY_EXEC SphericalHarmonics(int legendre_order) : m_legendre_order(legendre_order) {}
+    DRAY_EXEC ~SphericalHarmonics()
+    {
+      delete_buffer();
+    }
+
+    /** Evaluates all spherical harmonics up to legendre_order. */
+    template <typename T>
+    DRAY_EXEC
+    const T* eval_all(const dray::Vec<T, 3> &xyz_normal);
+
+    /** Calls eval_all() and performs dot product. */
+    template <typename T>
+    DRAY_EXEC
+    T eval_function(const T * coefficients, const dray::Vec<T, 3> &xyz_normal)
+    {
+      return eval_function<T>(m_legendre_order,
+                              coefficients,
+                              eval_all(xyz_normal));
+    }
+
+    /** Calls eval_all() and accumulates vector to coefficients. */
+    template <typename T>
+    DRAY_EXEC
+    void project_point(T * coefficients,
+                       const dray::Vec<T, 3> &xyz_normal,
+                       const T integration_value,
+                       const T integration_weight)
+    {
+      project_point<T>(m_legendre_order,
+                       coefficients,
+                       eval_all(xyz_normal),
+                       integration_value,
+                       integration_weight);
+    }
+
+    DRAY_EXEC int num_harmonics() const { return num_harmonics(m_legendre_order); }
+
+
+    DRAY_EXEC static int index(int n, int m) { return n * (n+1) + m; }
+    DRAY_EXEC static int alp_index(int n, int m) { return n * (n+1) / 2 + m; }
+    // alp = associated legendre polynomial, only uses m >= 0.
+
+    DRAY_EXEC static int num_harmonics(int legendre_order)
+    {
+      return (legendre_order+1)*(legendre_order+1);
+    }
+
+    /** Static version does not call eval_all().
+     *  Good for evaluating different functions
+     *  with different sets of coefficients. */
+    template <typename T>
+    DRAY_EXEC
+    static T eval_function(const int legendre_order,
+                         const T * coefficients,
+                         const T * sph_harmonics)
+    {
+      T value = 0.0f;
+      const int Np1_sq = num_harmonics(legendre_order);
+      for (int nm = 0; nm < Np1_sq; ++nm)
+        value += coefficients[nm] * sph_harmonics[nm];
+      return value;
+    }
+
+    /** Static version does not call eval_all().
+     *  Good for projecting different integration values
+     *  to different sets of coefficients. */
+    template <typename T>
+    DRAY_EXEC
+    static void project_point(const int legendre_order,
+                              T * coefficients,
+                              const T * sph_harmonics,
+                              const T integration_value,
+                              const T integration_weight)
+    {
+      const int Np1_sq = num_harmonics(legendre_order);
+      const T integration_product = integration_value * integration_weight;
+      for (int nm = 0; nm < Np1_sq; ++nm)
+        coefficients[nm] += sph_harmonics[nm] * integration_product;
+    }
+
+  private:
+    template <typename T>
+    DRAY_EXEC T * resize_buffer(const size_t size);
+    DRAY_EXEC void delete_buffer();
+
+  private:
+    int m_legendre_order = 0;
+    size_t m_buffer_size = 0;
+    char * m_buffer = nullptr;
+};
+
+
+Array<Float> integrate_moments(Array<Vec<Float,3>> &destinations,
+                               int32 _legendre_order,
+                               Array<Float> &path_lengths,
+                               Array<Vec<Float,3>> &ray_sources,
+                               Array<int32> &source_cells,
+                               Float _cell_volume,
+                               LowOrderField *emission)
+{
+  using sph_t = Float;
+
+  const int32 ncomp = path_lengths.ncomp();
+  const int32 legendre_order = _legendre_order;
+  const int32 num_moments = (legendre_order + 1) * (legendre_order + 1);
+  const int32 num_destinations = destinations.size();
+  const int32 num_sources = ray_sources.size();
+
+  Array<Float> destination_moments;
+  destination_moments.resize(num_destinations * num_moments, ncomp);
+
+  const Float cell_volume = _cell_volume;
+
+  ConstDeviceArray<Vec<Float, 3>> destinations_dev(destinations);
+  ConstDeviceArray<Vec<Float, 3>> ray_sources_dev(ray_sources);
+  ConstDeviceArray<Float> path_lengths_dev(path_lengths);
+  ConstDeviceArray<int32> source_cells_dev(source_cells);
+  ConstDeviceArray<Float> emission_dev(emission->values());
+
+  NonConstDeviceArray<Float> destination_moments_dev(destination_moments);
+
+  RAJA::forall<for_policy> (RAJA::RangeSegment(0, num_destinations),
+      [=] DRAY_LAMBDA (int32 dest)
+  {
+    // Clear output.
+    for (int32 nm = 0; nm < num_moments; ++nm)
+      for (int32 component = 0; component < ncomp; ++component)
+        destination_moments_dev.get_item(num_moments * dest + nm, component) = 0.0f;
+
+    SphericalHarmonics sph(legendre_order);
+
+    // For each source
+    //   For each component
+    //     For each moment
+    //       Multiply-and-accumulate source term with spherical harmonic.
+    const Vec<Float, 3> dest_pos = destinations_dev.get_item(dest);
+    for (int32 source = 0; source < num_sources; ++source)
+    {
+      const Vec<Float, 3> omega = (dest_pos - ray_sources_dev.get_item(source));
+      const Vec<Float, 3> omega_hat = omega.normalized();
+      const Float rcp_mag2 = rcp_safe(omega.magnitude2());
+      // Really should use volume-average (over source cell) of rcp_mag2.
+
+      const sph_t * sph_eval = sph.eval_all<sph_t>(omega_hat);
+
+      const int32 source_idx = source_cells_dev.get_item(source);
+
+      for (int32 component = 0; component < ncomp; ++component)
+      {
+        const Float dEmission_dV = emission_dev.get_item(source_idx, component);
+        const Float source_dL_dOmega
+          = dEmission_dV * cell_volume * rcp_mag2;
+
+        const Float transmitted = path_lengths_dev.get_item(
+            num_sources * dest + source, component);
+
+        const Float trans_source = transmitted * source_dL_dOmega;
+
+        for (int32 nm = 0; nm < num_moments; ++nm)
+        {
+          const sph_t spherical_harmonic = sph_eval[nm];
+          destination_moments_dev.get_item(num_moments * dest + nm, component)
+              += spherical_harmonic * trans_source;
+        }//moments
+      }//components
+    }//sources
+  });//destinations
+
+  return destination_moments;
+}
+
+
+template <typename T>
+DRAY_EXEC
+T * SphericalHarmonics::resize_buffer(const size_t size)
+{
+  size_t new_size = sizeof(T) * size;
+  if (m_buffer_size < new_size)
+  {
+    if (m_buffer != nullptr)
+      delete [] m_buffer;
+    m_buffer = new char[new_size];
+  }
+  return (T*)(m_buffer);
+}
+
+
+DRAY_EXEC
+void SphericalHarmonics::delete_buffer()
+{
+  if (m_buffer != nullptr)
+    delete [] m_buffer;
+}
+
+template <typename T>
+DRAY_EXEC
+const T* SphericalHarmonics::eval_all(const dray::Vec<T, 3> &xyz_normal)
+{
+  // Computed using the recursive formulation in Appendix A1 in
+  //
+  //     @inproceedings{sloan2008stupid,
+  //       title={Stupid spherical harmonics (sh) tricks},
+  //       author={Sloan, Peter-Pike},
+  //       booktitle={Game developers conference},
+  //       volume={9},
+  //       pages={42},
+  //       year={2008}
+  //     }
+
+  // Note: I came up with a recursive form of the normalization constants K_n^m.
+  //   The formula for K_n^m involves ratios of factorials. I used floats
+  //   because the ratios do not simply to integers. I haven't studied the stability
+  //   properties of evaluating them directly or recursively, so no guarantees.
+  //   Also, to test the normalization constants you need to do a reconstruction,
+  //   not just evaluate each spherical harmonic individually.
+
+  const int Np1 = m_legendre_order + 1;
+  const int Np1_sq = Np1 * Np1;
+  const int result_sz = Np1_sq;            // result
+  const int sin_sz = Np1;                  // sine
+  const int cos_sz = Np1;                  // cosine
+  const int alp_sz = Np1 * (Np1+1) / 2;    // associated legendre polynomial
+  const int k2_sz = Np1 * (Np1+1) / 2;     // square of normalization constant
+
+  T * const buffer = resize_buffer<T>(result_sz + sin_sz + cos_sz + alp_sz + k2_sz);
+
+  T * const resultp = buffer;
+  T * const sinp = resultp + result_sz;
+  T * const cosp = sinp + sin_sz;
+  T * const alpp = cosp + cos_sz;
+  T * const k2p = alpp + alp_sz;
+
+  const T sqrt2 = sqrtl(2);
+
+  const T &x = xyz_normal[0];
+  const T &y = xyz_normal[1];
+  const T &z = xyz_normal[2];
+
+  // m=0
+  {
+    const int m = 0;
+
+    sinp[m] = 0;
+    cosp[m] = 1;
+
+    // n == m
+    alpp[alp_index(m, m)] = 1;
+    k2p[alp_index(0, 0)] = 1.0 / (4 * dray::pi());
+    resultp[index(m, m)] = sqrt(k2p[alp_index(m, m)]) * alpp[alp_index(m, m)];
+    /// resultp[index(m, m)] = Knm(m, m) * alpp[alp_index(m, m)];
+
+    // n == m+1
+    if (m+1 <= m_legendre_order)
+    {
+      alpp[alp_index(m+1, m)] = (2*m+1) * z * alpp[alp_index(m, m)];
+      k2p[alp_index(1, 0)] = 2 * (1+1) / (4 * dray::pi());
+      resultp[index(m+1, m)] = sqrt(k2p[alp_index(m+1, m)]) * alpp[alp_index(m+1, m)];
+      /// resultp[index(m+1, m)] = Knm(m+1, m) * alpp[alp_index(m+1, m)];
+    }
+
+    // n >= m+2
+    for (int n = m+2; n <= m_legendre_order; ++n)
+    {
+      alpp[alp_index(n, m)] = ( (2*n-1) * z * alpp[alp_index(n-1, m)]
+                               -(n+m-1)     * alpp[alp_index(n-2, m)] ) / (n-m);
+
+      k2p[alp_index(n, 0)] = (2*n+1) / (4 * dray::pi());
+
+      resultp[index(n, m)] = sqrt(k2p[alp_index(n, m)]) * alpp[alp_index(n, m)];
+      /// resultp[index(n, m)] = Knm(n, m) * alpp[alp_index(n, m)];
+    }
+  }
+
+  // m>0
+  for (int m = 1; m <= m_legendre_order; ++m)
+  {
+    sinp[m] = x * sinp[m-1] + y * cosp[m-1];
+    cosp[m] = x * cosp[m-1] - y * sinp[m-1];
+
+    // n == m
+    alpp[alp_index(m, m)] = (1-2*m) * alpp[alp_index(m-1, m-1)];;
+    k2p[alp_index(m, m)] = k2p[alp_index(m-1, m-1)] * (2*m+1) / ((2*m-1) * (2*m-1) * (2*m));
+    resultp[index(m, m)] = sqrt(2*k2p[alp_index(m, m)]) * cosp[m] * alpp[alp_index(m, m)];
+    /// resultp[index(m, m)] = sqrt2*Knm(m, m) * cosp[m] * alpp[alp_index(m, m)];
+
+    // n == m+1
+    if (m+1 <= m_legendre_order)
+    {
+      alpp[alp_index(m+1, m)] = (2*m+1) * z * alpp[alp_index(m, m)];
+      k2p[alp_index(m+1, m)] =
+          k2p[alp_index((m+1)-1, m)] * (2*(m+1)+1) * ((m+1)-m) / ((2*(m+1)-1) * ((m+1)+m));
+
+      resultp[index(m+1, m)] = sqrt(2*k2p[alp_index(m+1, m)]) * cosp[m] * alpp[alp_index(m+1, m)];
+      /// resultp[index(m+1, m)] = sqrt2*Knm(m+1, m) * cosp[m] * alpp[alp_index(m+1, m)];
+    }
+
+    // n >= m+2
+    for (int n = m+2; n <= m_legendre_order; ++n)
+    {
+      alpp[alp_index(n, m)] = ( (2*n-1) * z * alpp[alp_index(n-1, m)]
+                               -(n+m-1)     * alpp[alp_index(n-2, m)] ) / (n-m);
+
+      k2p[alp_index(n, m)] = k2p[alp_index(n-1, m)] * 2*(n+1) * (n-m) / ((2*n-1) * (n+m));
+
+      resultp[index(n, m)] = sqrt(2*k2p[alp_index(n, m)]) * cosp[m] * alpp[alp_index(n, m)];
+      /// resultp[index(n, m)] = sqrt2*Knm(n, m) * cosp[m] * alpp[alp_index(n, m)];
+    }
+  }
+
+  // m<0
+  for (int m = -1; m >= -m_legendre_order; --m)
+  {
+    const int absm = -m;
+    for (int n = absm; n <= m_legendre_order; ++n)
+    {
+      resultp[index(n, m)] = sqrt(2*k2p[alp_index(n, absm)]) * sinp[absm] * alpp[alp_index(n, absm)];
+      /// resultp[index(n, m)] = sqrt2*Knm(n, absm) * sinp[absm] * alpp[alp_index(n, absm)];
+    }
+  }
+
+  return resultp;
+}
+
+
 
 
 };//namespace dray
