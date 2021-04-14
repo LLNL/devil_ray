5385934318d13638b93eb8fc567217ab3caaf5a6
Matt Larsen <larsen30@llnl.gov>
2021-04-14 14:31:21 -0700

    code for multiple topos inside low order imports

--- src/dray/io/blueprint_low_order.hpp
+++ src/dray/io/blueprint_low_order.hpp
@@ -18,16 +18,19 @@ class BlueprintLowOrder
 public:
 
   static DataSet import(const conduit::Node &n_dataset);
-  static DataSet import_uniform(const conduit::Node &n_coords,
-                                Array<int32> &conn,
-                                int32 &n_elems,
-                                std::string &shape);
+  static
+  std::shared_ptr<TopologyBase> import_uniform(const conduit::Node &n_coords,
+                                               Array<int32> &conn,
+                                               int32 &n_elems,
+                                               std::string &shape);
 
-  static DataSet import_explicit(const conduit::Node &n_coords,
-                                 const conduit::Node &n_topo,
-                                 Array<int32> &conn,
-                                 int32 &n_elems,
-                                 std::string &shape);
+
+  static
+  std::shared_ptr<TopologyBase> import_explicit(const conduit::Node &n_coords,
+                                                const conduit::Node &n_topo,
+                                                Array<int32> &conn,
+                                                int32 &n_elems,
+                                                std::string &shape);
 
 };
 
