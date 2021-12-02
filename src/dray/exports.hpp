c660578d25be9239ba66fa17bdf2d4a6fa2bdccd
Cyrus Harrison <cyrush@llnl.gov>
2021-12-02 15:29:11 -0500

    missing include and mfem mpi tweak

--- src/dray/exports.hpp
+++ src/dray/exports.hpp
@@ -6,6 +6,8 @@
 #ifndef DRAY_EXPORTS_HPP
 #define DRAY_EXPORTS_HPP
 
+#include <RAJA/RAJA.hpp>
+
 #if defined(__CUDACC__) && !defined(DEBUG_CPU_ONLY)
 
 #define DRAY_CUDA_ENABLED
@@ -13,7 +15,7 @@
 #define DRAY_EXEC_ONLY RAJA_INLINE RAJA_DEVICE
 #define DRAY_LAMBDA RAJA_DEVICE
 
-#elif define(__HIP_PLATFORM_HCC__) && !defined(DEBUG_CPU_ONLY)
+#elif defined(__HIP_PLATFORM_HCC__) && !defined(DEBUG_CPU_ONLY)
 
 #define DRAY_HIP_ENABLED
 #define DRAY_EXEC RAJA_INLINE RAJA_HOST_DEVICE
