cb2f1f95712e6d4484fde2fa760b4b3dd4f61491
Cyrus Harrison <cyrush@llnl.gov>
2021-12-03 11:07:55 -0800

    dray cmake mpi adj

--- src/cmake/SetupBLT.cmake
+++ src/cmake/SetupBLT.cmake
@@ -42,13 +42,41 @@ include(${BLT_SOURCE_DIR}/SetupBLT.cmake)
 if(ENABLE_MPI)
     # on some platforms (mostly cray systems) folks skip mpi
     # detection in BLT by setting ENABLE_FIND_MPI = OFF
-    # in these cases, we need to set FOUND_MPI = TRUE,
-    # since the rest of our cmake logic to include MPI uses FOUND_MPI
+    # in these cases, we need to set MPI_FOUND = TRUE,
+    # since the rest of our cmake logic to include MPI uses MPI_FOUND
     if(NOT ENABLE_FIND_MPI)
-        set(FOUND_MPI ON CACHE BOOL "")
+        set(MPI_FOUND ON CACHE BOOL "")
+    endif()
+
+    # adjust MPI from BLT
+    if( ${CMAKE_VERSION} VERSION_LESS "3.15.0" )
+        # older cmake, we use BLT's mpi support, it uses 
+        # the name mpi
+        set(dray_blt_mpi_deps mpi CACHE STRING "")
+        set(DRAY_USE_CMAKE_MPI_TARGETS FALSE CACHE BOOL "")
+    else()
+        # if we are using BLT's enable mpi, then we must
+        # make sure the MPI targets exist
+        if(ENABLE_FIND_MPI)
+            # our import logic needs this info if hdf5 depends
+            # on mpi
+            if(TARGET MPI::MPI_CXX)
+                set(DRAY_USE_CMAKE_MPI_TARGETS TRUE CACHE BOOL "")
+                message(STATUS "Using MPI CMake imported target: MPI::MPI_CXX")
+                # newer cmake we use find mpi targets directly
+                set(dray_blt_mpi_deps MPI::MPI_CXX CACHE STRING "")
+            else()
+                message(FATAL_ERROR "Cannot use CMake imported targets for MPI."
+                                    "(CMake > 3.15, ENABLE_MPI == ON, but "
+                                    "MPI::MPI_CXX CMake target is missing.)")
+            endif()
+        else()
+            set(DRAY_USE_CMAKE_MPI_TARGETS FALSE CACHE BOOL "")
+            # compiler will handle them implicitly
+            set(dray_blt_mpi_deps "" CACHE STRING "")
+        endif()
     endif()
 endif()
-
 ################################################################
 # apply folders to a few ungrouped blt targets
 ################################################################
